deab111a1f29dd799da3b0955ed70482ed516f3a
tiinaVaisanen <tiina.vaisanen@awacademy.training>
2022-08-30 11:13:09 +0300

    updated apu.test.js

--- rss-lambda/apu.test.js
+++ rss-lambda/apu.test.js
@@ -1,16 +1,25 @@
 jest.mock('./apu.js');
-const { fetchLatest } = require('./apu.js');
+const { luoAikaleima, fetchLatest } = require('./apu.js');
 
 
 test('should get rss feed', async () => {
-    const items = { title: 'test', contentSnippet: 'test 2' };
+    const items = { title: 'testititle', contentSnippet: 'test 2' };
     const feed = { title: 'test', items: items }; // expected 
-    fetchLatest.mockImplementation(() => ({ title: 'test', contentSnippet: 'test 2' }),
+    fetchLatest.mockImplementation(() => ({ title: 'test', items: { title: 'testititle', contentSnippet: 'test 2'}}),
     ); // received
     
     const result = await fetchLatest();
 
     //expect(result.description).toEqual(feedConfig.description);
-    expect(result).toEqual(feed.items);
-    //expect(result.items).toEqual(items);
+    expect(result).toEqual(feed);
+    expect(result.items).toEqual(items);
+})
+
+test('testaa aikaleima', async () => {
+    const aikaleima = '2022-08-30t11.00'; // expected 
+    luoAikaleima.mockImplementation(() => ('2022-08-30t11.00')); // received
+    
+    const result = luoAikaleima();
+
+    expect(result).toEqual(aikaleima);
 })
